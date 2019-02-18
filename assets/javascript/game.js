bfc294c858c555941f161c13e6c02c91e4cd0b56
onix-xcc <onix.xcc@gmail.com>
2019-02-18 16:46:43 -0600

    Added coloring and text to the html file, began the js

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+//Array of letters
+var letterChoicesComputer = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+// Keeping scores and tracking guesses
+
+var wins = 0;
+var losses = 0;
+var guessesLeft = 9;
+var guessesSoFar = [];
+var computer = [];
+
+/
